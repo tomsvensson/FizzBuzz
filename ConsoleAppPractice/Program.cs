ceddfcb2b73bd9259c1bbe168a2a39ad925915e1
Tom Svensson <tom_svensson@outlook.com>
2024-07-31 12:04:43 +0200

    Add project files.

---
+++ ConsoleAppPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hej världen!");
+
+for (int i = 0; i < 100; i++)
+{
+
+    if (((i + 1) % 3 == 0) && ((i + 1) % 5 == 0))
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if ((i + 1) % 3 == 0) {
+
+        Console.WriteLine("Fizz");
+    }
+    else if ((i + 1) % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+
+    }
+    else Console.WriteLine(i + 1);
+    }
+
